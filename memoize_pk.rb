44811c5b72630a2b2a419a98aab74412a995984e
Philip Kobernik <ptk921@gmail.com>
2012-12-17 00:17:51 -0800

    ghost method insertion via class_eval (not coupled)

--- memoize_pk.rb
+++ memoize_pk.rb
@@ -11,18 +11,29 @@ class Discounter
   end
 end
 
-d = Discounter.new
+# method memoize creates a ghost class and uses class_eval to inject a method into it
+# dynamically created method is automatically a subclass of whatever object is passed in
+#
+# Like: seperate concerns, not coupled
+# Dislike:
+#
+def memoize object, method
+  ghost = class << object; self; end
+  ghost.class_eval do
+    memory ||= {}
 
-def d.discount(*skus)
-  @memory ||= {}
-
-  if @memory.has_key?(skus)
-    @memory[skus]
-  else
-    @memory[skus] = super(*skus)
+    define_method(method) do |*args|
+      if memory.has_key?(args)
+        memory[args]
+      else
+        memory[args] = super(*args)
+      end
+    end
   end
-
 end
+
+d = Discounter.new
+memoize d, :discount
 
 puts d.discount(1, 2, 3)
 puts d.discount(1, 2, 3)
