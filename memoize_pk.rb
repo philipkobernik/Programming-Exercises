3d4bc081f92bc733a95f3cde628f6952ff314fc8
Philip Kobernik <ptk921@gmail.com>
2012-12-17 00:24:29 -0800

    using the rewrite method (coupled)

--- memoize_pk.rb
+++ memoize_pk.rb
@@ -11,29 +11,21 @@ class Discounter
   end
 end
 
-# method memoize creates a ghost class and uses class_eval to inject a method into it
-# dynamically created method is automatically a subclass of whatever object is passed in
-#
-# Like: seperate concerns, not coupled
-# Dislike:
-#
-def memoize object, method
-  ghost = class << object; self; end
-  ghost.class_eval do
-    memory ||= {}
+class Discounter
+  alias_method :_original_discount_, :discount
 
-    define_method(method) do |*args|
-      if memory.has_key?(args)
-        memory[args]
-      else
-        memory[args] = super(*args)
-      end
+  def discount(*skus)
+    @memory ||= {}
+
+    if @memory.has_key?(skus)
+      @memory[skus]
+    else
+      @memory[skus] = _original_discount_(*skus)
     end
   end
 end
 
 d = Discounter.new
-memoize d, :discount
 
 puts d.discount(1, 2, 3)
 puts d.discount(1, 2, 3)
