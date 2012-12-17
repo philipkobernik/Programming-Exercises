acf8c1f0725b238264fcda739cad30d46c8441aa
Philip Kobernik <ptk921@gmail.com>
2012-12-16 23:59:10 -0800

    ghost class method (coupled)

--- memoize_pk.rb
+++ memoize_pk.rb
@@ -11,29 +11,18 @@ class Discounter
   end
 end
 
-# defines a method #memoize that dynamically creates a subclass of 'klass' and defines a method
-# that will return a dictionary value or call super to memoize the result
-#
-# Like: separation of concerns
-# Dislike: interface -- creating objects via the memoize method feels strange
-#
-def memoize klass, method
-  Class.new(klass) do
-    memory = {}
+d = Discounter.new
 
-    define_method method do |*args|
-      if memory.has_key?(args)
-        memory[args]
-      else
-        memory[args] = super(*args)
-      end
-    end
+def d.discount(*skus)
+  @memory ||= {}
+
+  if @memory.has_key?(skus)
+    @memory[skus]
+  else
+    @memory[skus] = super(*skus)
   end
 
 end
-
-
-d = memoize(Discounter, :discount).new
 
 puts d.discount(1, 2, 3)
 puts d.discount(1, 2, 3)
