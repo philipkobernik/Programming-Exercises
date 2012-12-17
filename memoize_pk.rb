8eb3bad9f04ae7ac693a274142c362feee0f449e
Philip Kobernik <ptk921@gmail.com>
2012-12-17 01:08:37 -0800

    rewrite pattern using module (not coupled)

--- memoize_pk.rb
+++ memoize_pk.rb
@@ -1,10 +1,15 @@
+# module Memoize defines method #remember that stores the state of the method specified,
+# then dynamically rewrites that method using define_method.
+# The re-written method, if need be, can call the original method by binding it to self
+#
+# LIKE: don't have to use alias_method to 'hide' the original method. Module makes re-use easy. Interface is nice and concise.
+# DISLIKE: replacing the original method feels un-safe (brazen) to me
+#
 module Memoize
 
   def remember(method)
-    original = "original #{method}"
 
-    alias_method original, method
-
+    original_method = instance_method(method)
     memory = {}
 
     define_method(method) do |*args|
@@ -12,7 +17,8 @@ module Memoize
       if memory.has_key?(args)
         memory[args]
       else
-        memory[args] = send "original #{method}", *args
+        bound_method = original_method.bind(self)
+        memory[args] = bound_method.call(*args)
       end
     end
   end
